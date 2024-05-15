1b6fc50bbc2c0ea4d4093e2f3e14ae8fc6fae3ba
QhYamamoto <kazuma.yamamoto819@gmail.com>
2024-05-15 20:49:41 +0900

    change colorscheme

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,16 +1,17 @@
 vim.cmd("let g:netrw_liststyle = 3")
+vim.cmd("let g:gruvbox_material_transparent_background = 1")
 
 -- register autocmds
 vim.api.nvim_create_augroup("lua", {})
 
 vim.api.nvim_create_autocmd({
-	"InsertLeave",
-	"CmdlineLeave",
+  "InsertLeave",
+  "CmdlineLeave",
 }, {
-	group = "lua",
-	callback = function()
-		os.execute("$zenhan 0 > /tmp/output 2>&1")
-	end,
+  group = "lua",
+  callback = function()
+    os.execute("$zenhan 0 > /tmp/output 2>&1")
+  end,
 })
 
 -- vim options
@@ -24,15 +25,15 @@ opt.tabstop = 2
 opt.shiftwidth = 2
 opt.expandtab = true
 opt.autoindent = true
-opt.list = true -- make all white chars visible
+opt.list = true   -- make all white chars visible
 opt.listchars = { -- setting for each white chars
-	tab = "|-",
-	space = "･",
-	trail = "･",
-	eol = "↲",
-	extends = "»",
-	precedes = "«",
-	nbsp = "%",
+  tab = "|-",
+  space = "･",
+  trail = "･",
+  eol = "↲",
+  extends = "»",
+  precedes = "«",
+  nbsp = "%",
 }
 
 -- search settings
@@ -51,8 +52,8 @@ opt.splitright = true
 opt.splitbelow = true
 
 opt.whichwrap:append({
-	["<"] = true,
-	[">"] = true,
-	["["] = true,
-	["]"] = true,
+  ["<"] = true,
+  [">"] = true,
+  ["["] = true,
+  ["]"] = true,
 })
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,11 +1,7 @@
 return {
-	"cpea2506/one_monokai.nvim",
-	priority = 1000,
-	config = function()
-		require("one_monokai").setup({
-			transparent = true,
-		})
-
-		vim.cmd("colorscheme one_monokai")
-	end,
+  "sainnhe/gruvbox-material",
+  priority = 1000,
+  config = function()
+    vim.cmd("colorscheme gruvbox-material")
+  end,
 }
