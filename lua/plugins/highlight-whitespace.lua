1f23cfdc8523839fa7fa7fee78dcab579ff27ebd
QhYamamoto <kazuma.yamamoto819@gmail.com>
2024-05-16 15:34:55 +0900

    fix highlight-whitespace settings

--- lua/plugins/highlight-whitespace.lua
+++ lua/plugins/highlight-whitespace.lua
@@ -1,15 +1,14 @@
 return {
-	"lukoshkin/highlight-whitespace",
-	config = true,
-	opts = {
-		palette = {
-			other = {
-				tws = "PaleVioletRed",
-				["\\S\\@<=\\s,\\@="] = "coral1",
-				["\\S\\@<=\\(#\\|--\\)\\@<! \\{2,3\\}\\S\\@=\\(#\\|--\\)\\@!"] = "LightGoldenrod3",
-				["\\(#\\|--\\)\\@<= \\{2,\\}\\S\\@="] = "#3B3B3B",
-				["\\S\\@<= \\{3,\\}\\(#\\|--\\)\\@="] = "#3B3B3B",
-			},
-		},
-	},
+  "lukoshkin/highlight-whitespace",
+  config = true,
+  opts = {
+    palette = {
+      other = {
+        tws = "PaleVioletRed",
+        ["\\S\\@<=\\s,\\@="] = "coral1",
+        ["\\(#\\|--\\)\\@<= \\{2,\\}\\S\\@="] = "#3B3B3B",
+        ["\\S\\@<= \\{3,\\}\\(#\\|--\\)\\@="] = "#3B3B3B",
+      },
+    },
+  },
 }
