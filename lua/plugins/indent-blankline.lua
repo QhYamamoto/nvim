a16dbe5c0d49c85c467fcab666509bde3aa47e18
QhYamamoto <kazuma.yamamoto819@gmail.com>
2024-05-16 15:32:41 +0900

    fix indent blankline settings

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,34 +1,9 @@
 return {
-	"lukas-reineke/indent-blankline.nvim",
-	event = { "BufReadPre", "BufNewFile" },
-	main = "ibl",
-	opts = {
-		indent = { char = "┊" },
-	},
-	config = function()
-		local highlight = {
-			"RainbowRed",
-			"RainbowYellow",
-			"RainbowBlue",
-			"RainbowOrange",
-			"RainbowGreen",
-			"RainbowViolet",
-			"RainbowCyan",
-		}
-
-		local hooks = require("ibl.hooks")
-		-- create the highlight groups in the highlight setup hook, so they are reset
-		-- every time the colorscheme changes
-		hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-			vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#DBAFB3" })
-			vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E0C38D" })
-			vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#79B9ED" })
-			vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
-			vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
-			vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
-			vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
-		end)
-
-		require("ibl").setup({ indent = { highlight = highlight } })
-	end,
+  "lukas-reineke/indent-blankline.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  main = "ibl",
+  opts = {
+    indent = { char = "┊" },
+  },
+  config = function() require("ibl").setup() end,
 }
