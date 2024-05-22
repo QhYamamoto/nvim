1417b61d9a5ee42a721eb071073260df5213f9e8
QhYamamoto <kazuma.yamamoto819@gmail.com>
2024-05-22 18:04:42 +0900

    edit cmd for diffview

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -5,5 +5,32 @@ return {
 
     keymap.set("n", "<leader>gdo", "<cmd>:DiffviewOpen<CR>", { desc = "Open diff view (HEAD~1..HEAD)" })
     keymap.set("n", "<leader>gdc", "<cmd>:DiffviewClose<CR>", { desc = "Close diff view" })
+
+    local function get_git_rev_from_clipboard()
+      local rev = vim.fn.getreg('+') -- get string from clipboard
+      if rev:match('^[0-9a-fA-F]+$') then
+        return rev
+      else
+        return nil
+      end
+    end
+
+    function DiffViewOpenWithGitRev()
+      local rev = get_git_rev_from_clipboard()
+      if rev then
+        local cmd = ':DiffviewOpen ' .. rev .. '~1..' .. rev
+        print(rev)
+        vim.cmd(cmd)
+      else
+        print("Invalid HashID on clipboard.")
+      end
+    end
+
+    keymap.set(
+      'n',
+      '<Leader>gdr',
+      '<cmd>lua DiffViewOpenWithGitRev()<CR>',
+      { desc = "Open diff view ({HashId on clipboard}~1..{HashID on clipboard})", noremap = true, silent = true }
+    )
   end,
 }
