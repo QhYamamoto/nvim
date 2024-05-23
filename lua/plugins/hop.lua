efa727afc0558e8c0a3ecc2caa4d463f159ab790
QhYamamoto <kazuma.yamamoto819@gmail.com>
2024-05-23 11:08:21 +0900

    add hop

---
+++ lua/plugins/hop.lua
@@ -0,0 +1,27 @@
+return {
+  "phaazon/hop.nvim",
+  branch = "v2",
+  config = function()
+    local hop = require("hop")
+
+    hop.setup({
+      keys = "etovxqpdygfblzhckisuran"
+    })
+
+    local keymap = vim.keymap
+
+    local directions = require('hop.hint').HintDirection
+    keymap.set('', 'f', function()
+      hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
+    end, { remap = true })
+    keymap.set('', 'F', function()
+      hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
+    end, { remap = true })
+    keymap.set('', 't', function()
+      hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })
+    end, { remap = true })
+    keymap.set('', 'T', function()
+      hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })
+    end, { remap = true })
+  end
+}
