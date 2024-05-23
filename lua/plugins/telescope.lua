6ce5db345b0f783cb14318fd93c149d9213a84bc
QhYamamoto <kazuma.yamamoto819@gmail.com>
2024-05-23 10:59:55 +0900

    edit telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,32 +6,53 @@ return {
     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
     "nvim-tree/nvim-web-devicons",
     "folke/todo-comments.nvim",
+    "nvim-telescope/telescope-project.nvim",
+    "ThePrimeagen/harpoon"
   },
   config = function()
     local telescope = require("telescope")
     local actions = require("telescope.actions")
 
+    local project_actions = require("telescope._extensions.project.actions")
     telescope.setup({
+      extensions = {
+        project = {
+          base_dirs = {
+            "~/dev/toppan-kumagaya",
+            "~/dev/integritis"
+          },
+          theme = "dropdown",
+          order_by = "asc",
+          search_by = "title",
+          sync_with_nvim_tree = true,
+          on_project_selected = function(prompt_bufnr)
+            project_actions.change_working_directory(prompt_bufnr)
+            vim.cmd("SessionRestore")
+            require("harpoon.ui").nav_file(1)
+          end
+        }
+      },
       defaults = {
         path_display = { "smart" },
         mappings = {
           i = {
             ["<C-k>"] = actions.move_selection_previous,
-            ["<C-j>"] = actions.move_selection_next, 
-            ["<C-p>"] = actions.send_selected_to_qflist + actions.open_qflist,
+            ["<C-j>"] = actions.move_selection_next,
           },
         },
       }
     })
 
     telescope.load_extension("fzf")
+    telescope.load_extension("project")
 
     local keymap = vim.keymap
 
     keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>", { desc = "Fuzzy find files in cwd" })
-    keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<CR>", { desc = "Fuzzy find recent files" })
+    keymap.set("n", "<leader>fr", "<cmd>Telescope resume<CR>", { desc = "Fuzzy find with cache" })
     keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<CR>", { desc = "Find string in cwd" })
     keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<CR>", { desc = "Find string under cursor in cwd" })
+    keymap.set("n", "<leader>fp", "<cmd>Telescope project<CR>", { desc = "Find project" })
     keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<CR>", { desc = "Find todos" })
-   end
+  end
 }
